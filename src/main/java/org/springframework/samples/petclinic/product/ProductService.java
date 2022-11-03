b21c81807c05b65ab2e3522ae3a97ec1f98587e0
andrecouto40 <andrecouto40@gmail.com>
2022-11-03 01:20:32 +0100

    First Commit

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,22 +2,36 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+    @Autowired
+    ProductRepository productRepository;
+   
     public List<Product> getAllProducts(){
-        return null;
+        return productRepository.findAll();
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        return productRepository.findByPriceLessThan(price);
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        return productRepository.findByNameProductType(typeName);
     }
 
     public Product save(Product p){
         return null;       
     }
 
+    public ProductType findByNameProductType(String name){
+        return productRepository.findByNameProductType(name);
+    }
+
+    public List<ProductType> findAllProductTypes(){
+        return productRepository.findAllProductTypes();
+    }
     
 }
